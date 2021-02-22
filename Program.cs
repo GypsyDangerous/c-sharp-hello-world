136ee64e2dd198de379a9819f3a26126f9193fdd
David Snyder <davidgraygs4@gmail.com>
2021-02-21 23:11:21 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			bool continueRunning = false;
+			do {
+				Console.WriteLine("Enter the first number: \r");
+				int number1 = Convert.ToInt32(Console.ReadLine()); 
+				Console.WriteLine("Enter the second number: \r");
+				int number2 = Convert.ToInt32(Console.ReadLine()); 
+			} while (continueRunning == true);
+        }
+    }
+}
